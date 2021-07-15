99e8e74b91eb32efc68e94911f1c70bfe6245e8b
root <root@devops-sandbox.localdomain>
2021-07-14 23:56:05 -0400

    minor changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World v2");
+		get("/hello", (req, res) -> "Hello World v3");
 	}
 }
