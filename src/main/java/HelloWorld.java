e50ffd3afe8250cc54f726c9774bfa4ba649cd15
root <root@docker-build.localdomain>
2021-07-22 02:39:23 -0400

    v1.4

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World v1.3");
+		get("/hello", (req, res) -> "Hello World v1.4");
 	}
 }
